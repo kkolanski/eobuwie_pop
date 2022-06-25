e887b1a25801d6d883f6049330b69abd3705ab28
Karol Kolanski <gralu86@gmail.com>
2022-06-25 13:25:58 +0200

    First registration Page method addeD

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -1,7 +1,18 @@
 from base_page import BasePage
+from selenium.webdriver.common.by import By
+
+class RegistrationPageLocators:
+    """
+    Locators for Registration Page
+    """
+    REGISTER_LINK = (By.XPATH, '//a[@data-testid="header-register-link"]')
 
 class RegistrationPage(BasePage):
     """
     Registration Page
     """
-    pass
+    def click_register_link(self):
+        """
+        Clicks Register link
+        """
+        self.driver.find_element(*RegistrationPageLocators.REGISTER_LINK).click()
